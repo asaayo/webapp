5033f75be2a2a75e53ddb0fd000ed8a82c1b76c0
Brandon <Brandon@BoxTheSecond>
2013-10-08 07:22:43 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/default.css" media="screen and (min-device-width: 481px)">
+        <link rel="stylesheet" type="text/css" href="css/mobile.css" media="only screen and (-webkit-min-device-pixel-ratio: 2)">
+        <link href='http://fonts.googleapis.com/css?family=Titillium+Web' rel='stylesheet' type='text/css'>
+        
+        
+    </head>
+    <body>
+        <div id="header">
+       	<h1 class="left">A website by a guy</h1>
+        <p class="righty">An experience in webdev</p>
+        </div>
+        <div id="headerbar" class="extendfull">
+            <a href="index.php">Main page</a>
+            <a href="">Link 2</a>
+            <a href="">Link 3</a>
+        </div>
+        <div id="mainbody">
+            <p>
+                I'm using this to host whatever projects I happen to be working on at any given time. 
+                Right now it's a combination of learning HTML5 and working on my capstone project.
+            </p>
+            <p>
+                &emsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mollis mi ante, vel interdum turpis vehicula quis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lacinia, elit eleifend pharetra vestibulum, leo tellus malesuada est, vitae eleifend nulla urna ac lectus. Proin erat velit, varius sit amet ipsum et, sollicitudin elementum dolor. Duis lobortis velit risus, ac suscipit libero congue imperdiet. Praesent pharetra, nisi eu interdum tempus, justo leo pretium nunc, at tincidunt enim nulla at nunc. Aliquam pharetra vel tellus non semper. Sed ut ornare elit. Donec placerat est non vestibulum pellentesque. Maecenas et blandit nibh. Quisque quis aliquam justo. Duis euismod eleifend mauris in viverra. Cras pretium interdum metus et elementum. In ut consectetur justo, in semper enim. In tempor, sem at vestibulum fermentum, augue enim feugiat elit, sed fringilla justo quam non metus.
+            </p>
+            <p style="float:right">
+                <img src="assets/code.jpeg" align="left"style="height:400px; padding-right:10px;"/>
+                &emsp;Etiam et eleifend quam. Nulla non tempus nibh, suscipit laoreet dui. Vivamus eleifend a est fringilla malesuada. Cras a leo quis metus feugiat vulputate. Proin nisl nisl, sollicitudin a nulla vel, feugiat elementum libero. Nam vestibulum metus a justo pretium, sit amet venenatis dui commodo. In sit amet sem imperdiet, faucibus erat nec, bibendum urna. Curabitur vitae pellentesque purus, a euismod dolor. Donec sed est sed eros facilisis gravida. Maecenas id risus et purus laoreet fermentum sed et urna. Morbi condimentum mi a tellus pellentesque, et sodales massa tristique. Morbi euismod nisi placerat commodo interdum. Phasellus quis eleifend urna. 
+                <br/><br/>&emsp;Duis mattis tellus ut fringilla congue. Etiam ornare turpis vulputate gravida luctus. Phasellus sit amet risus sed urna venenatis ultrices. Nunc a augue rutrum, convallis sem vel, viverra nisi. Pellentesque id vulputate neque, at bibendum urna. Suspendisse sit amet sapien ligula. Nam at nibh id augue auctor lacinia id viverra diam. Fusce at ultrices lectus. Nam tempus sodales suscipit. Quisque pharetra dapibus tristique. Nullam commodo, elit at lacinia varius, lacus purus dignissim purus, quis feugiat quam diam quis sapien. Nam aliquam dolor sit amet augue elementum accumsan. Phasellus a tincidunt erat. In ultricies feugiat nibh, sit amet tincidunt arcu rutrum vitae. 
+            </p>
+        </div>
+        <footer>
+            <div id="footerbar" class="extendfull">
+                Footer content &copy; a guy<br/>
+                By reading this, your soul becomes property of a guy.
+            </div>
+        </footer>
+    </body>
+</html>
