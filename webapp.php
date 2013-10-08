be31419aa466b7f719772b931b21ceb009aeee9a
Brandon <Brandon@Lapbox>
2013-10-08 13:17:17 -0400

    Location data and first maps available

--- webapp.php
+++ webapp.php
@@ -8,17 +8,34 @@
     }
     $ip = $_SERVER["REMOTE_ADDR"];
     $info= geolocation($ip);
-    foreach($info as $key => $value){
-        echo "$key 0:  $value <br/>";
-    }
 ?>
 <!doctype HTML>
 <html>
-    <title>WebApp</title>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" type="text/css" href="css/default.css" media="screen and (min-device-width: 481px)">
-        <link rel="stylesheet" type="text/css" href="css/mobile.css" media="only screen and (-webkit-min-device-pixel-ratio: 2)">
+    <title>WebApp</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <link rel="stylesheet" type="text/css" href="css/default.css" media="screen and (min-device-width: 481px)"/>
+        <link rel="stylesheet" type="text/css" href="css/mobile.css" media="only screen and (-webkit-min-device-pixel-ratio: 2)"/>
+        <script>
+            //findLocation takes two functions as its arguments
+            function findLocation(){
+                navigator.geolocation.getCurrentPosition(foundLocation, noLocation);
+            }
+            //foundLocation is used if location information is available
+            function foundLocation(position){
+                var lat = position.coords.latitude;
+                var lon = position.coords.longitude;
+                document.getElementById("Latitude").value=lat;
+                document.getElementById("Longitude").value=lon;
+                var url = "http://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon + "&zoom=12&size=400x400&sensor=false&";
+                url += "markers=color:red%7C" + lat + "," + lon;
+                document.getElementById("Map").src = url;
+            }
+            //else noLocation is used
+            function noLocation(){
+                alert('Location information unavailable');
+            }
+        </script>
     </head>
     <body>
         <div id="header">
@@ -39,7 +56,15 @@
                     <option value='VZ'>Verizon</option>
                     <option value='SP'>Sprint</option>
                 </select>
+                <br/>
+                <input type="text" name="Latitude" id="Latitude" value="">
+                <br/>
+                <input type="text" name="Longitude" id="Longitude" value="">
+                <br/>
+                <input type="button" value='Find Me' onclick='javascript:findLocation();'/>
+                <img id ="Map" src="" />
             </form>
+            
         </div>
         <footer>
             <div id="footerbar" class="extendfull">
@@ -47,5 +72,8 @@
                 By reading this, your soul becomes property of a guy.
             </div>
         </footer>
+        <?php
+            echo "Zipcode : $info[zipcode]";
+        ?>
     </body>
 </html>
