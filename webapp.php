8840613fec9febb21f06c460aea00d88865d2711
Brandon <Brandon@Lapbox>
2013-11-19 13:21:28 -0500

    Google places integration started

--- webapp.php
+++ webapp.php
@@ -24,7 +24,15 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <link rel="stylesheet" type="text/css" href="css/default.css" media="screen and (min-device-width: 481px)"/>
         <link rel="stylesheet" type="text/css" href="css/mobile.css" media="only screen and (-webkit-min-device-pixel-ratio: 2)"/>
+        <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?libraries=places&sensor=true"></script>
         <script>
+            var success = false;
+            var loc1;
+            var loc2;
+            var map;
+            var service;
+            var infowindow;
+            
             //findLocation takes two functions as its arguments
             function findLocation(){
                 navigator.geolocation.getCurrentPosition(foundLocation, noLocation);
@@ -33,16 +41,17 @@
             function foundLocation(position){
                 var lat = position.coords.latitude;
                 var lon = position.coords.longitude;
-                var loc1 = lat+","+lon;
+                loc1 = lat+","+lon;
                 var lat2 = <?= $info['latitude'];?>;
                 var lon2 = <?= $info['longitude'];?>;
-                var loc2 = lat2+","+lon2;
+                loc2 = lat2+","+lon2;
                 document.getElementById("latitude").value=lat;
                 document.getElementById("longitude").value=lon;
                 var url1 = "http://maps.googleapis.com/maps/api/staticmap?center=" + loc1 + "&zoom=12&size=400x400&sensor=false&markers=color:red%7C" + loc1;
                 var url2 = "http://maps.googleapis.com/maps/api/staticmap?center=" + loc2 + "&zoom=12&size=400x400&sensor=false&markers=color:red%7C" + loc2;
                 document.getElementById("map1").src = url1;
                 document.getElementById("map2").src = url2;
+                success=true;
             }
             //else noLocation is used
             function noLocation(){
@@ -62,6 +71,30 @@
                         break;
                 }
             }
+            function toNext(){
+                document.getElementById("mainbody").style.display=("none");
+                document.getElementById("p2").style.display=("inline");
+                document.getElementById("p2").className ="bodyclass";
+            }
+            /*
+            function initialize(){
+                if(success)
+                    var curLoc = new google.maps.LatLng(loc1);
+                else
+                    var curLoc = new google.maps.LatLng(loc2);
+                    map = new google.maps.Map(document.getElementById('map'),{center: curLoc;zoom: 15;});
+                    var request = {location: curLoc;radius: '5000';types: ['restaurant']};
+                    service = new google,maps.places.PlacesService(map);
+                    service.nearbySearch(request, callback);   
+            }
+            function callback(results, status){
+                if(status == google.maps.places.PlacesServiceStats.OK){
+                    for(var i = 0; i < results.length; i++){
+                        var place = results[i];
+                        createMarker(results[i]);
+                    }   
+                }
+            }*/
         </script>
     </head>
     <body>
@@ -74,7 +107,7 @@
             <a href="webapp.php">WebApp</a>
             <a href="assets/resume.pdf">R&eacute;sum&eacute;</a>
         </div>
-        <div id="mainbody">
+        <div id="mainbody" class="bodyclass">
             <p>
                 This part of the application gets user location via HTML5's geolocation
             </p>
@@ -88,6 +121,11 @@
                 <img id="map1" class="lefty" src="" />
                 <img id="map2" class="righty" src="" />
             </form>
+            <br style="clear:both"/>
+            <p>Find restaurants near me:</P>
+            <input type="button" value="Find Restaurants" id="restaurants" onclick="javascript:toNext();"/>
+        </div>
+        <div id="p2">
             
         </div>
         <footer>
